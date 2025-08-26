ef484e9ab2d5704c87903f1b01d819bc8cc90fb0
Jan Jaap de Groot <info@janjaap.com>
2025-08-26 14:08:38 +0200

    Update Package.swift with setup for embedded dependencies

--- Package.swift
+++ Package.swift
@@ -10,29 +10,18 @@ let package = Package(
     products: [
         .library(
             name: "AbraSDK",
-            targets: ["AbraSDKFramework", "AbraSDKWrapper"]
+            targets: ["AbraSDKFramework"]
         ),
     ],
     dependencies: [
-        // Only include dependencies that the binary framework directly needs
-        .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0")
+        // All dependencies are embedded in the binary
     ],
     targets: [
-        // Binary target with URL to the released framework
         .binaryTarget(
             name: "AbraSDKFramework",
             url: "https://nbg1.your-objectstorage.com/abrabv/AbraSDK/1.1.0/AbraSDK-1.1.0.zip",
             checksum: "6b218b5b170dfbc63a50fef915284b3bef0b5f07f1837fa1adfb621fdf1c481c"
         ),
-        // Wrapper target to handle binary target dependencies
-        .target(
-            name: "AbraSDKWrapper",
-            dependencies: [
-                .product(name: "RswiftLibrary", package: "R.swift"),
-                "AbraSDKFramework"
-            ],
-            path: "Wrapper"
-        )
     ],
     swiftLanguageVersions: [.v5]
 )
