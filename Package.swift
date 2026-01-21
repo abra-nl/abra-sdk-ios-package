ed09b44430687473d5bb528c62aac328b5277ac2
Jan Jaap de Groot <info@janjaap.com>
2026-01-21 11:24:09 +0100

    Re-release 1.2.3

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Binary target containing the framework
         .binaryTarget(
             name: "AbraSDKFramework",
-            path: "releases/1.2.2/AbraSDK-1.2.2.zip"
+            path: "releases/1.2.3/AbraSDK-1.2.3.zip"
         ),
         // Wrapper target to handle binary target dependencies
         .target(
