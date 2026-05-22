a08d86019dcf70080e24b32d6e0b0d00b9d920c3
Jan Jaap de Groot <info@janjaap.com>
2026-05-22 10:25:48 +0200

    Remove `R.swift` dependency from `Package.swift`

--- Package.swift
+++ Package.swift
@@ -8,30 +8,15 @@ let package = Package(
         .iOS(.v17)
     ],
     products: [
-        // Library with dependencies
         .library(
             name: "AbraSDK",
-            targets: ["AbraSDKFramework", "AbraSDKWrapper"]
+            targets: ["AbraSDKFramework"]
         ),
     ],
-    dependencies: [
-        // Dependencies that the binary framework needs
-        .package(url: "https://github.com/mac-cain13/R.swift.git", .upToNextMajor(from: "7.7.0")),
-    ],
     targets: [
-        // Binary target containing the framework
         .binaryTarget(
             name: "AbraSDKFramework",
             path: "releases/1.4.0/AbraSDK-1.4.0.zip"
-        ),
-        // Wrapper target to handle binary target dependencies
-        .target(
-            name: "AbraSDKWrapper",
-            dependencies: [
-                .product(name: "RswiftLibrary", package: "R.swift"),
-                "AbraSDKFramework"
-            ],
-            path: "Wrapper"
         )
     ],
     swiftLanguageVersions: [.v5]
